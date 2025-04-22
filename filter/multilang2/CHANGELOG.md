19b7c61f88f9d1eeac09012b44f4a4f5b23a1319
José A. García <joaghix@gmail.com>
2025-04-22 11:29:26 +0200

    Actualizado filtro multilang2

--- filter/multilang2/CHANGELOG.md
+++ filter/multilang2/CHANGELOG.md
@@ -7,11 +7,18 @@ and this project adheres to [Semantic Versioning](https://semver.org/spec/v2.0.0
 
 ## [Unreleased]
 
+## [2.0.5.2] - 2025.04.17
+
+### Changed
+
+- Updated the copyright notices to remove Moodle, and use filter_multilang2 instead. See https://moodle.org/mod/forum/discuss.php?d=467516 for the rationale.
+- Ignore phpcs codesniff that check for standard Moodle copyright boilerplate.
+
 ## [2.0.5.1] - 2024.12.12
 
 ### Fixed
 
-- [#56] Updated Basque translations for latest changes (I forgot to do it in 2.00.5)
+- [#56] Updated Basque translations for latest changes (I forgot to do it in 2.0.5)
 
 ## [2.0.5] - 2024.11.27
 
--- filter/multilang2/version.php
+++ filter/multilang2/version.php
@@ -1,32 +1,32 @@
 <?php
-// This file is part of Moodle - http://moodle.org/
+// This file is part of filter_multilang2 - https://moodle.org/
 //
-// Moodle is free software: you can redistribute it and/or modify
-// it under the terms of the GNU General Public License as published by
-// the Free Software Foundation, either version 3 of the License, or
-// (at your option) any later version.
+// filter_multilang2 is free software: you can redistribute it and/or
+// modify it under the terms of the GNU General Public License as
+// published by the Free Software Foundation, either version 3 of the
+// License, or (at your option) any later version.
 //
-// Moodle is distributed in the hope that it will be useful,
-// but WITHOUT ANY WARRANTY; without even the implied warranty of
-// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+// filter_multilang2 is distributed in the hope that it will be
+// useful, but WITHOUT ANY WARRANTY; without even the implied warranty
+// of MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
 // GNU General Public License for more details.
 //
 // You should have received a copy of the GNU General Public License
-// along with Moodle.  If not, see <http://www.gnu.org/licenses/>.
+// along with filter_multilang2.  If not, see <https://www.gnu.org/licenses/>.
 
 /**
  * Version details
  *
  * @package    filter_multilang2
- * @copyright  1999 onwards Martin Dougiamas (http://dougiamas.com)
+ * @copyright  1999 onwards Martin Dougiamas (https://dougiamas.com)
  *             2015 onwards Iñaki Arenaza & Mondragon Unibertsitata
- * @license    http://www.gnu.org/copyleft/gpl.html GNU GPL v3 or later
+ * @license    https://www.gnu.org/copyleft/gpl.html GNU GPL v3 or later
  */
 
 defined('MOODLE_INTERNAL') || die();
 
-$plugin->version   = 2024121201;          // The current plugin version (Date: YYYYMMDDXX).
+$plugin->version   = 2025041701;          // The current plugin version (Date: YYYYMMDDXX).
 $plugin->requires  = 2021051700;          // Requires this Moodle version.
 $plugin->component = 'filter_multilang2'; // Full name of the plugin (used for diagnostics).
-$plugin->release   = '2.0.5.1';
+$plugin->release   = '2.0.5.2';
 $plugin->maturity  = MATURITY_STABLE;
